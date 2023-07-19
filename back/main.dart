36cf385568118b23bd63f2e441d0bfc3dc1cecc2
revenantonthemission <102278079+revenantonthemission@users.noreply.github.com>
2023-07-19 21:21:40 +0900

    Update main.dart

--- back/main.dart
+++ back/main.dart
@@ -5,41 +5,39 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:intl/intl.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 List<dynamic> listfood = [];
 Map name = {};
-Map category ={};
-Map trav_time ={};
+Map category = {};
+Map trav_time = {};
 Map tag = {};
 Map listmeta = {};
 
-
-
-void makelist(var parsed_list){
+void makelist(var parsed_list) {
   int idx = 1;
-  for(var i in parsed_list){
+  for (var i in parsed_list) {
     name[i["name"]] = idx;
-    if(category.containsKey(i["category"])){
+    if (category.containsKey(i["category"])) {
       category[i["category"]].add(idx);
-    }else{
+    } else {
       category[i["category"]] = <int>[];
       category[i["category"]].add(idx);
     }
-    if(trav_time.containsKey(i["trav_time"])){
+    if (trav_time.containsKey(i["trav_time"])) {
       trav_time[i["trav_time"]].add(idx);
-    }else {
+    } else {
       trav_time[i["trav_time"]] = <int>[];
       trav_time[i["trav_time"]].add(idx);
     }
 
-    for(var j in i["tag"]){
-      if(tag.containsKey(j)){
+    for (var j in i["tag"]) {
+      if (tag.containsKey(j)) {
         tag[j].add(idx);
-      }else{
+      } else {
         tag[j] = <int>[];
         tag[j].add(idx);
       }
@@ -48,18 +46,32 @@ void makelist(var parsed_list){
   }
 }
 
-
-
 Future<int> init(CounterStorage cs) async {
   bool result = await InternetConnectionChecker().hasConnection;
-  if(result == true) {
+  if (result == true) {
+    print("Internet Connected");
     try {
-
       /*
         fetch data from firebase
       */
-
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      print("Firebase Initialized");
+      final storageReference = FirebaseStorage.instance
+          .refFromURL("gs://wffl-52c4d.appspot.com/food.json");
+      try {
+        const oneMegabyte = 1024 * 1024;
+        final data = await storageReference.getData(oneMegabyte);
+        final String dataAsString = utf8.decode(data!);
+        print(dataAsString);
+        // Data for "images/island.jpg" is returned, use this as needed.
+      } on FirebaseException catch (e) {
+        print("@@@");
+        // Handle any errors.
+      }
       final directory = await getApplicationDocumentsDirectory();
+
       final path = directory.path;
       final file = File('$path/food.json');
       final meta_file = File('$path/meta_food.json');
@@ -69,15 +81,34 @@ Future<int> init(CounterStorage cs) async {
       listmeta = jsonDecode(meta);
       makelist(listfood);
 
-
-
       return 0;
-    }catch(e){
+    } catch (e) {
+      print("Error");
       return -1;
     }
   } else {
     try {
+      /*
+        fetch data from firebase
+      */
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      print("Firebase Initialized2");
+      final storageReference = FirebaseStorage.instance
+          .refFromURL("gs://wffl-52c4d.appspot.com/food.json");
+      try {
+        const oneMegabyte = 1024 * 1024;
+        final data = await storageReference.getData(oneMegabyte);
+        final String dataAsString = utf8.decode(data!);
+        print(dataAsString);
+        // Data for "images/island.jpg" is returned, use this as needed.
+      } on FirebaseException catch (e) {
+        print("@@@2");
+        // Handle any errors.
+      }
       final directory = await getApplicationDocumentsDirectory();
+
       final path = directory.path;
       final file = File('$path/food.json');
       final meta_file = File('$path/meta_food.json');
@@ -87,9 +118,9 @@ Future<int> init(CounterStorage cs) async {
       listmeta = jsonDecode(meta);
       makelist(listfood);
 
-
       return 0;
-    }catch(e){
+    } catch (e) {
+      print("Error2");
       return -1;
     }
   }
@@ -97,14 +128,10 @@ Future<int> init(CounterStorage cs) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  if(init(CounterStorage())==-1){
+  print("Hello World");
+  if (init(CounterStorage()) == -1) {
     throw 'Error';
   }
-
-
   runApp(
     MaterialApp(
       title: 'I Copied the flutter demo page lol',
@@ -114,7 +141,6 @@ void main() async {
 }
 
 class CounterStorage {
-
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
@@ -124,8 +150,6 @@ class CounterStorage {
     final path = await _localPath;
     return File('$path/counter.json');
   }
-
-
 }
 
 class FlutterDemo extends StatefulWidget {
@@ -140,8 +164,6 @@ class FlutterDemo extends StatefulWidget {
 class _FlutterDemoState extends State<FlutterDemo> {
   int _counter = 0;
   var _LocalContent;
-
-
 
   int _incrementCounter() {
     setState(() {
